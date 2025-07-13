1bb93a4294da91e19fbc84db3ae867e2adafc5e9
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-13 09:45:40 +0600

    js comparison

---
+++ comparison.js
@@ -0,0 +1,47 @@
+/**
+ * COMPARISON
+ * 1. >
+ * 2. <
+ * 3. ==
+ * 4. >=
+ * 5. <=
+ * 6. !=
+ * 
+ * ----------------------------
+ * 7. && AND
+ * 8. || OR
+ */
+
+
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(10 >= 10);
+// console.log(15 >= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 == 10);
+
+// -----------------------------
+
+// var peyaraPrice = 40;
+// var applePrice = 250;
+
+// console.log(peyaraPrice > applePrice);
+// console.log(applePrice < peyaraPrice);
+
+
+/**
+ * 1. ==  -->  ONLY COMPARES TWO VALUES. DON'T CARE ABOUT THE DATA TYPE
+ * 2. ===  --> COMPARES BOTH THE VALUES AND DATA TYPES
+ */
+
+// console.log(10 == 10);
+// console.log(10 == '10');
+// console.log(10 === '10');
+// console.log(120 === 'tomato');
+// -----------------------------
+
+console.log(10 != '10');
+console.log(10 !== '10');
