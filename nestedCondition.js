bf80c3637a80b0b93e0161516246f876b8c6c200
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-13 11:48:32 +0600

    nested condition

---
+++ nestedCondition.js
@@ -0,0 +1,17 @@
+const money = -10;
+if(money > 300) {
+  console.log('Bro you are rich!!!');
+}
+else {
+  if(money > 100) {
+    console.log('You are neither poor nor rich');
+  }
+  else {
+    if(money > 0) {
+      console.log('Eat a Banana and boost up your calcium.');
+    }
+    else {
+      console.log('You are bankrupt.');
+    }
+  }
+}
