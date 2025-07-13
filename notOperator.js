77e0a5a71169c099d902c6f89ba0cd514f50dd6e
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-13 12:31:20 +0600

    logical not operator

---
+++ notOperator.js
@@ -0,0 +1,16 @@
+const isLeader = true;
+// if(isLeader) {
+//   console.log('Yes');
+// }
+// else {
+//   console.log('No');
+// }
+
+const isPassed = true;
+// if(isPassed === false)
+if(!isPassed) {
+  console.log('Bad');
+}
+else{
+  console.log('Good');
+}
