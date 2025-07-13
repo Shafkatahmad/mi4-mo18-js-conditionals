6d0bccff4c86596de5e6df19e4feedeaf24d01d9
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-13 10:05:39 +0600

    if else

---
+++ if.js
@@ -0,0 +1,52 @@
+/**
+ * If you feed me, 
+ * I will go
+ * 
+ * ----------------------------
+ * if you buy food for me
+ *        I will arrive
+ * 
+ * ----------------------------
+ * if pizzaPrice less than 300/-
+ *        I will buy
+ * 
+ * 
+ * --------STURCTURE-----------
+ * if(condition) {
+ *      
+ * }
+ * 
+ * ----------------------------
+ */
+
+
+// if(3 < 10) {
+//   console.log('I have a smaller value');
+// }
+
+// var pizzaPrice = 500;
+// if(pizzaPrice < 300) {
+//   console.log('Give me a Pizza');
+// }
+// pizzaPrice = 250; // 50% OFF on pizza
+// if(pizzaPrice < 300) {
+//   console.log('Give me a Pizza');
+// }
+
+
+/**
+ * if(condition) {
+ *      execute code if condition above is true
+ * }
+ * else {
+ *      execute code if the condition is false
+ * }
+ */
+
+var weight = 40;
+if(weight < 20) {
+  console.log('I will carry it by myself');
+}
+else {
+  console.log('I will rent a car');
+}
