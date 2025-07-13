0240636ab5a068469c6d80b0a11fe6eea40b078e
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-13 09:15:48 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,22 @@
+/**
+ * VARIABLE ==> container
+ */
+
+// number type
+var sunglassPrice = 80;
+
+// string type
+var subject = "EEE";
+
+// boolean
+var passed = true;
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with a number 
+ * 5. can not be a keyword
+ * 6. prefered camelCase
+ */
