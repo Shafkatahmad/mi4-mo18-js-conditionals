2b92048701742e4bbcad99e7e6b2818c88ee3173
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-13 09:19:49 +0600

    revised

--- variable.js
+++ variable.js
@@ -20,3 +20,14 @@ var passed = true;
  * 5. can not be a keyword
  * 6. prefered camelCase
  */
+
+var lastExamResult = 89;
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. += , -=, *=, /=
+ * 3. isNaN
+ * 4. null
+ * 5. Number
+ */
